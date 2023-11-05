26f2cee919e80216fb1648a07219427f6336516f
trong12344 <trongmap2k3>
2023-11-05 13:17:22 +0700

    backend

---
+++ src/test/java/com/Beelab/BackendBeelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Beelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBeelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
